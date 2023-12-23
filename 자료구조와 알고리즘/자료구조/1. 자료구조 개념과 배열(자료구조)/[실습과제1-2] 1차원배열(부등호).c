831ac2f09f0b2a4de547a6ec642a8d480199a045
kimsiyeon0223 <ksyeon0223@gmail.com>
2023-12-24 00:17:36 +0900

    오류수정

--- 자료구조와 알고리즘/자료구조/1. 자료구조 개념과 배열(자료구조)/[실습과제1-2] 1차원배열(부등호).c
+++ 자료구조와 알고리즘/자료구조/1. 자료구조 개념과 배열(자료구조)/[실습과제1-2] 1차원배열(부등호).c
@@ -1,23 +1,25 @@
 #include <stdio.h>
+#define MAX_SIZE 100
+
 int main() {
-	int i, j, tmp, n, com[n];
-	scanf("%d", &n);
-	for(i=1; i<=n; i++){
-		scanf("%d", &com[i]);
-	}
-	for(i=1; i<=n; i++){
-		printf("%d: ", i);
-		for(j=1; j<=n; j++){
-			if(i==j)
-				continue;
-			if(com[i] > com[j])
-				printf("> ");
-			else if(com[i] < com[j]) 
-				printf("< ");
-			else 
-				printf("= ");
-		}
-		printf("\n");
-	}
-	return 0;
+    int i, j, tmp, n, com[MAX_SIZE];
+    scanf("%d", &n);
+    for(i=1; i<=n; i++){
+        scanf("%d", &com[i]);
+    }
+    for(i=1; i<=n; i++){
+        printf("%d: ", i);
+        for(j=1; j<=n; j++){
+            if(i==j)
+                continue;
+            if(com[i] > com[j])
+                printf("> ");
+            else if(com[i] < com[j]) 
+                printf("< ");
+            else 
+                printf("= ");
+        }
+        printf("\n");
+    }
+    return 0;
 }
